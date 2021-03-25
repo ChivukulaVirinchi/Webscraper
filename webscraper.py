f319c05994bc462b7a07ebf5491b202107778acf
ChivukulaVirinchi <virinchi@gurujada.com>
2021-03-25 19:13:12 +0530

    webscraper workig for erpnext

--- webscraper.py
+++ webscraper.py
@@ -3,41 +3,68 @@ import requests
 import urllib
 import os
 import pdfkit
-url = 'https://www.bankexamstoday.com/2015/08/banking-awareness-questions-pdf.html'
+import re
+
+url = 'https://docs.erpnext.com/docs/user/manual/en'
 response = requests.get(url)
 soup = bs(response.text,'html.parser')
-a=soup.findAll('a',{'class':'pdf'})
+a=soup.findAll('a',{'class':'stretched-link'})
+
+
+lb1 = soup.findAll('a',{'href': re.compile('^/')})
+lb2 = soup.findAll('link',{'href': re.compile('^/')})
+lb3 = soup.findAll('script',{'src': re.compile('^/')})
+lb4 = soup.findAll('script',{'href': re.compile('https://')})
+lb5 = soup.findAll('link',{'href': re.compile('https://')})
+
+for i in lb1:
+    i['href'] = i['href'].replace('?ver=1616352305.528987', '') 
+    i['href'] = 'https://docs.erpnext.com' + i['href']
+for i in lb2:
+    i['href'] = i['href'].replace('?ver=1616352305.528987', '') 
+    i['href'] = 'https://docs.erpnext.com' + i['href']
+
+for i in lb3:
+    i['src'] =i['src'].replace('?ver=1616352305.528987', '') 
+    i['src'] = 'https://docs.erpnext.com' + i['src']
+
+for i in lb4:
+    i['href'] =i['href'].replace('?ver=1616352305.528987', '') 
+
+for i in lb5:
+    i['href'] =i['href'].replace('?ver=1616352305.528987', '') 
+
+
 
 for element in a:
- name = element['href'].split('/')[4]
+ name = element['href'].split('/')
+ if name[2] == 'frappe.io':
+     continue
+ else:  name = element['href'].split('/')[5]
+  
  link = element['href']
  directory = '/home/virinchi/code/python/python-beautiful-soup/webscraper/'
  print('saving : ',name)
- pdfFile = urllib.request.urlopen(link)
- file = open(directory+name, 'wb')
- file.write(pdfFile.read())
- file.close()
-grid_box = soup.findAll('div',{'class':'grid-box'})
-for i in range(1,len(grid_box)):
+ 
+card = soup.findAll('div',{'class':'card'})
+for i in range(1,len(card)):
     # stripping topic heading name from the grid-box   
-    dirname = grid_box[i].h2.text.strip()
+    dirname = card[i].h3.text.strip()
     print('creating folder for : ',dirname)
     
     # Creating a directory with same name as topic heading (replacing
     #spaces with underscore as spaces can create problem in creating folder)   
-    
-    dirname = '/home/virinchi/code/python/python-beautiful-soup/webscraper/'+dirname.replace(' ','_')
+    dirname = '/home/virinchi/code/'
     if not os.path.isdir(dirname):
         os.mkdir(dirname)
-links = (grid_box[i].findAll('a'))
-for f in links:
+    links = (card[i].findAll('a',{'href': re.compile('/docs/user/manual/en/*')}))
+    
+    for f in links:
         html_link = (f['href'])
-        html_name = f.text.replace(' ','_').strip()
-        html_res = requests.get(html_link)
-        
-    # creating files with same name as name in html link 
-        filename =  dirname+'/'+html_name+'.pdf'
-        
+        html_res = requests.get('https://docs.erpnext.com' + html_link)
+            
+        # creating files with same name as name in html link 
+        filename =  dirname+html_link+'.pdf'
         if not os.path.isfile(filename):
-            pdf = pdfkit.from_url(html_link,filename)
-            print('created_file : '+dirname+'/'+html_name+'.pdf'+' successfully')
+            pdf = pdfkit.from_url(html_res,filename)
+            print(filename)
